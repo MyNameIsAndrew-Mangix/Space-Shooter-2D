6a129df9958ef3fbdff0a01d50678716b5f68c3c
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2020-08-06 16:25:38 -0400

    Added New Enemy movement
    There's a 50% chance the default enemy(the one with normal colors) will move either straight down, or will move side to side.

--- SpaceShooter2D/Assets/Scripts/Enemy.cs
+++ SpaceShooter2D/Assets/Scripts/Enemy.cs
@@ -19,9 +19,18 @@ public class Enemy : MonoBehaviour
     private float _canFire = 2f;
     private float _fireRate;
     private bool _isAlive = true;
+    private int _coinFlip;
+    [SerializeField]
+    private float _cachedXPos;
+    [SerializeField]
+    private Vector3 _downAndLOrR;
+    [SerializeField]
+    private bool _hasHitCachedXPos = false;
     // Start is called before the first frame update
     void Start()
     {
+        _cachedXPos = transform.position.x + 2;
+        _coinFlip = UnityEngine.Random.Range(1, 3);
         _fireRate = UnityEngine.Random.Range(3.5f, 7f);
         _boxCollider2D = GetComponent<BoxCollider2D>();
         _player = GameObject.Find("Player").GetComponent<Player>();
@@ -57,15 +66,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyMovement();
+        EnemyMovement();
         if (Time.time > _canFire)
         {
             _canFire = Time.time + _fireRate;
-            enemyFire();
+            EnemyFire();
         }
     }
 
-    private void enemyFire()
+    private void EnemyFire()
     {
         if (_isAlive == true)
         {
@@ -75,16 +84,125 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void enemyMovement()
+    private void EnemyMovement()
     {
-        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
-
-        if (transform.position.y <= -5.44f)
+        switch (_coinFlip)
         {
-            float randomX = UnityEngine.Random.Range(-9.56f, 9.56f);
-            transform.position = new Vector3(randomX, 7.56f, 0);
+            case 1:
+                transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+                if (transform.position.y <= -5.44f)
+                {
+                    float randomX = UnityEngine.Random.Range(-9.56f, 9.56f);
+                    transform.position = new Vector3(randomX, 7.56f, 0);
+                }
+                break;
+            case 2:
+                _downAndLOrR.y = -1;
+                _downAndLOrR.x = MoveLeftOrRight(CheckToMoveLeftOrRight(_cachedXPos)); //CheckToMoveLeftOrRight(_cachedXPos);
+                transform.Translate(_downAndLOrR * _enemySpeed * Time.deltaTime);
+
+                if (transform.position.y <= -5.44f)
+                {
+                    float randomX = UnityEngine.Random.Range(-9.56f, 9.56f);
+                    _cachedXPos = randomX;
+                    transform.position = new Vector3(randomX, 7.56f, 0);
+                }
+                break;
+        }
+        if (transform.position.x >= 10.4)
+        {
+            transform.position = new Vector3(-10.2f, transform.position.y, 0);
+            _cachedXPos = transform.position.x + 2;
+        }
+        else if (transform.position.x <= -10.4f)
+        {
+            transform.position = new Vector3(10.2f, transform.position.y, 0);
+            _cachedXPos = transform.position.x - 2;
         }
 
+    }
+
+    private bool CheckToMoveLeftOrRight(float cachedXPos)
+    {
+        // return true for right
+        // return false for left
+        if (cachedXPos >= 0 && cachedXPos <= 9.56f)
+            return false;
+        else if (cachedXPos <= 0 && cachedXPos >= -9.56f)
+            return true;
+        else
+        {
+            Debug.LogError("CachedXPos out of range");
+            return false;
+        }
+    }
+
+
+
+    private int MoveLeftOrRight(bool LeftOrRight)
+    {
+        if (LeftOrRight && !_hasHitCachedXPos)
+        {
+            HitCachedPos();
+            return 1;
+        }
+        else if (LeftOrRight && _hasHitCachedXPos)
+        {
+            HitCachedPos();
+            return -1;
+        }
+        if (!LeftOrRight && !_hasHitCachedXPos)
+        {
+            HitCachedPos();
+            return -1;
+        }
+        else if (!LeftOrRight && _hasHitCachedXPos)
+        {
+            HitCachedPos();
+            return 1;
+        }
+        else
+        {
+            HitCachedPos();
+            Debug.LogError("CheckToMoveLeftOrRight has returned null");
+            return 0;
+        }
+    }
+
+    private void HitCachedPos()
+    {
+        float xPos = transform.position.x;
+
+        if (_hasHitCachedXPos && CheckToMoveLeftOrRight(_cachedXPos))
+        {
+            if (xPos <= (_cachedXPos - 2))
+            {
+                _hasHitCachedXPos = false;
+            }
+        }
+        else if (_hasHitCachedXPos && !CheckToMoveLeftOrRight(_cachedXPos))
+        {
+            if (xPos >= (_cachedXPos + 2))
+            {
+                _hasHitCachedXPos = false;
+            }
+        }
+
+        if (!_hasHitCachedXPos && CheckToMoveLeftOrRight(_cachedXPos))
+        {
+            if (xPos >= _cachedXPos)
+            {
+                _hasHitCachedXPos = true;
+            }
+        }
+        else if (!_hasHitCachedXPos && !CheckToMoveLeftOrRight(_cachedXPos))
+        {
+            if (xPos <= _cachedXPos)
+            {
+                _hasHitCachedXPos = true;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -109,6 +227,7 @@ public class Enemy : MonoBehaviour
             if (_player != null)
             {
                 _player.Damage();
+                EnemyDie();
             }
         }
     }
