a9e2ff79b1d942a506434852982057b2b4793109
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2020-08-18 07:48:05 -0400

    Implemented wave system
    New waves start every 5 seconds after all enemies from the previous wave have been defeated.

--- SpaceShooter2D/Assets/Scripts/SpawnManager.cs
+++ SpaceShooter2D/Assets/Scripts/SpawnManager.cs
@@ -6,7 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
 
     [SerializeField] 
-    private GameObject[] _enemies;
+    private GameObject[] _enemyTypes;
     [SerializeField]
     private GameObject _enemyContainer;
     [SerializeField]
@@ -14,6 +14,23 @@ public class SpawnManager : MonoBehaviour
     private int _previousPowerUp;
     private float _powerUpCooldown;
     private bool _stopSpawning = false;
+
+    private float _timeBetweenWaves = 5f;
+    private float _countDown = 2;
+
+    [SerializeField]
+    private List<GameObject> _enemies = new List<GameObject>();
+    private int _enemiesInWave = 2;
+
+
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    }
+
+    private SpawnState _state = SpawnState.COUNTING;
 
     // Start is called before the first frame update
     void Start()
@@ -25,35 +42,74 @@ public class SpawnManager : MonoBehaviour
     public void StartSpawning()
     {
         //only pass in values this way if planned to be dynamic based on conditions elsewhere or to be tweaked in the editor
-        StartCoroutine(SpawnEnemyRoutine(3.5f));
+        StartCoroutine(StartSpawner());
         StartCoroutine(SpawnPowerUpRoutine(_powerUpCooldown));
     }
 
-    IEnumerator SpawnEnemyRoutine(float waitTime)
+    IEnumerator StartSpawner()
     {
-        yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(_countDown);
 
         while (_stopSpawning == false)
         {
-            Vector3 enemySpawnPos = new Vector3(Random.Range(-9.56f, 9.56f), 8, 0);
-            int randomEnemy = Random.Range(0, 3);
-            GameObject newEnemy = Instantiate(_enemies[randomEnemy], enemySpawnPos, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(waitTime);
+            _state = SpawnState.SPAWNING;
+
+            yield return SpawnWave();
+
+            _state = SpawnState.WAITING;
+
+            yield return new WaitWhile(EnemyIsAlive);
+
+            _state = SpawnState.COUNTING;
+
+            yield return new WaitForSeconds(_timeBetweenWaves);
         }
+    }
+
+    private bool EnemyIsAlive()
+    {
+        if (_enemies.Count > 0)
+        {
+            for (int i = 0; i < _enemies.Count; i++)
+            {
+                GameObject enemyIndex = _enemies[i];
+                if (enemyIndex == null)
+                    _enemies.Remove(enemyIndex);
+            }
+            return true;
+        }
+        else
+            return false;
     }
 
     IEnumerator SpawnPowerUpRoutine(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-            while (_stopSpawning == false)
-            {
-                Vector3 powerUpSpawnPos = new Vector3(Random.Range(-9.56f, 9.56f), 8, 0);
-                int randomPowerUp = PreventSamePowerUp(RollPowerUp(), _previousPowerUp); //RandomWithExclusion(0, 6, _previousPowerUp);
-                _previousPowerUp = randomPowerUp;
-                Instantiate(_powerups[randomPowerUp], powerUpSpawnPos, Quaternion.identity);
-                yield return new WaitForSeconds(waitTime);
-            }
+        while (_stopSpawning == false)
+        {
+            Vector3 powerUpSpawnPos = new Vector3(Random.Range(-9.56f, 9.56f), 8, 0);
+            int randomPowerUp = PreventSamePowerUp(RollPowerUp(), _previousPowerUp); //RandomWithExclusion(0, 6, _previousPowerUp);
+            _previousPowerUp = randomPowerUp;
+            Instantiate(_powerups[randomPowerUp], powerUpSpawnPos, Quaternion.identity);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 enemySpawnPos = new Vector3(Random.Range(-9.56f, 9.56f), 8, 0);
+        int randomEnemy = Random.Range(0, 3);
+        _enemies.Add(Instantiate(_enemyTypes[randomEnemy], enemySpawnPos, Quaternion.identity));
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        _enemiesInWave++;
+        for (int i = 0; i < _enemiesInWave; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1.2f);
+        }
     }
 
     private int RollPowerUp()
